acf0d241159a1a1314388e8fe9f2f9a1f17523e3
devrenangabriel <devrenangabriel@gmail.com>
2025-09-24 21:00:51 -0300

    feat: add eventos page

--- src/app/(admin)/eventos/page.tsx
+++ src/app/(admin)/eventos/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function EventsPage() {
-  return <div>Events Page</div>;
+  return (
+    <div>
+      <Button
+        variant={"outline"}
+        className="w-full h-16 justify-start text-xl"
+        asChild
+      >
+        {/* TODO: Create constant for urls */}
+        <Link href="/eventos/novo">Cadastrar Novo evento</Link>
+      </Button>
+    </div>
+  );
 }
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -31,7 +31,7 @@ export default function AdminLayout({
         <CustomSidebar user={data.user} navMain={data.navMain} />
         <main className="w-full">
           <SidebarTrigger />
-          {children}
+          <div className="p-4 md:p-16">{children}</div>
         </main>
       </SidebarProvider>
     </ProtectedRoute>
