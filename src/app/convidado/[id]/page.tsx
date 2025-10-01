20bb89a9bde60ecd0da7a834d0fc0bce2ec0b7e2
devrenangabriel <devrenangabriel@gmail.com>
2025-10-01 20:40:47 -0300

    feat: add qr code

--- src/app/convidado/[id]/page.tsx
+++ src/app/convidado/[id]/page.tsx
@@ -1,5 +1,14 @@
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/lib/firebase";
+import qrcode from "qrcode";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+async function confirm(formData: FormData) {
+  "use server"; // This is the directive that makes this function a Server Action
+
+  console.log(formData.get("confirmarPresenca"));
+}
 
 type ConvidadoPageProps = {
   params: { id: string };
@@ -20,14 +29,19 @@ type ConvidadoPageProps = {
  * @param {ConvidadoPageProps} props - Objeto contendo os parâmetros da rota e os parâmetros de busca.
  * @returns {Promise<JSX.Element>} Um componente React com os detalhes do convidado ou mensagens de erro.
  */
-export default async function ConvidadoPage({ params, searchParams }: ConvidadoPageProps) {
+export default async function ConvidadoPage({
+  params,
+  searchParams,
+}: ConvidadoPageProps) {
   const { id } = params;
   const eventoId = searchParams.eventoId;
 
   if (!eventoId) {
     return (
       <main className="flex items-start justify-center min-h-screen">
-        <p className="text-red-500">⚠️ É necessário informar o eventoId na URL</p>
+        <p className="text-red-500">
+          ⚠️ É necessário informar o eventoId na URL
+        </p>
       </main>
     );
   }
@@ -46,24 +60,50 @@ export default async function ConvidadoPage({ params, searchParams }: ConvidadoP
 
   const convidado = snapshot.data();
 
+  const qrCode = await qrcode.toDataURL(
+    `https://seu-dominio.com/convidado/${id}?eventoId=${eventoId}`
+  );
+
   return (
     <main className="flex items-start justify-center min-h-screen bg-gray-50">
       <div className="bg-white p-6 rounded-2xl shadow-md w-full max-w-md space-y-4">
-        <h1 className="text-2xl font-bold">Olá, {convidado.titulo} {convidado.nome}</h1>
+        <h1 className="text-2xl font-bold">
+          Olá, {convidado.titulo} {convidado.nome}
+        </h1>
         <div className="text-gray-600">
-            Você foi convidado para o Evento {"Teste"}.
+          Você foi convidado para o Evento {"Teste"}.
         </div>
         <div className="space-y-2 text-sm">
-
           <p className="font-bold text-gray-600">Local: {convidado.cidade}</p>
           <p className="font-bold text-gray-600">Data: {convidado.data}</p>
-
         </div>
 
-        <div className="mt-4 p-4 border border-gray-300 rounded-lg flex flex-col gap-2">
-            <p>Esse QR Code é a sua credencial de acesso.</p>
-        </div>
+        <form
+          className="mt-4 p-4 border border-gray-300 rounded-lg flex flex-col gap-2"
+          action={confirm}
+        >
+          <p>Esse QR Code é a sua credencial de acesso.</p>
 
+          {typeof qrCode === "string" && (
+            <img
+              src={qrCode}
+              alt="QR Code"
+              className="w-full max-w-sm mx-auto"
+            />
+          )}
+
+          <Button>Confirmar Presença</Button>
+        </form>
+
+        <Button variant={"default"} asChild className="bg-blue-400 w-full">
+          <Link
+            href={"https://wa.me/55999999999"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Suporte
+          </Link>
+        </Button>
       </div>
     </main>
   );
