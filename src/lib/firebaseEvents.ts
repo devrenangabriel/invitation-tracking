85e76a1bc45ae35936b152afb30d3752eb493bf4
Vitoria Karolina Ferreira Sousa <49505117+vikarolina@users.noreply.github.com>
2025-10-23 07:40:37 -0300

    feat: add docstrings firebaseEvents

--- src/lib/firebaseEvents.ts
+++ src/lib/firebaseEvents.ts
@@ -7,7 +7,21 @@ export type Evento = {
   local: string;
   data: Date;
 };
-
+/**
+ * @summary Busca todos os eventos da coleção "eventos" no Firestore.
+ * @description Função assíncrona que se conecta ao Firestore, consulta
+ * a coleção "eventos", e mapeia os documentos recuperados para
+ * um array de objetos do tipo `Evento`.
+ *
+ * @returns {Promise<Evento[]>} Uma Promise que resolve com um array de `Evento`.
+ *
+ * @note
+ * Esta função lida especificamente com a conversão do campo `data`
+ * (que é um Timestamp do Firestore) para um objeto `Date` padrão do JavaScript
+ * usando o método `.toDate()`.
+ * Se o campo `data` for nulo ou indefinido no Firestore,
+ * ele atribui a data atual (`new Date()`) como fallback.
+ */
 export async function getEventos(): Promise<Evento[]> {
   const eventosRef = collection(db, "eventos");
   const snapshot = await getDocs(eventosRef);
